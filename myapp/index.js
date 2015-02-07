825a4058fa9cb0688786290849298cb2550a8747
Danubian <stjohnzack@yahoo.com>
2015-02-07 16:34:10 -0500

    Adding socket io

--- myapp/index.js
+++ myapp/index.js
@@ -1,30 +1,19 @@
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world')
-})
- 
-// Create a route that will respond to a POST request
-// app.post('/command', function(request, response) {
-//     // Get the command from the HTTP request and send it to the Minecraft
-//     // server process
-//     var command = request.param('Body');
-//     minecraftServerProcess.stdin.write(command+'\n');
- 
-//     // buffer output for a quarter of a second, then reply to HTTP request
-//     var buffer = [];
-//     var collector = function(data) {
-//         data = data.toString();
-//         buffer.push(data.split(']: ')[1]);
-//     };
-//     minecraftServerProcess.stdout.on('data', collector);
-//     setTimeout(function() {
-//         minecraftServerProcess.stdout.removeListener('data', collector);
-//         response.send(buffer.join(''));
-//     }, 250);
-// });
- 
-// Listen for incoming HTTP requests on port 3000
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
 app.listen(3000);
