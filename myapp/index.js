38369d18aea728fcd51e457aabb6f8d2215b490d
Danubian <stjohnzack@yahoo.com>
2015-02-07 16:36:23 -0500

    Removing added listen

--- myapp/index.js
+++ myapp/index.js
@@ -3,8 +3,6 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
-
 app.get('/', function (req, res) {
   res.send('hello world');
 });
